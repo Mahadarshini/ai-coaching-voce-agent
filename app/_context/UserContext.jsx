32bc51ca43732f6346bb0cf99a3d4defe7c801f9
Maha-kg <mahadarshini290306@gmail.com>
2025-04-07 15:20:51 +0530

    save user Info

---
+++ app/_context/UserContext.jsx
@@ -0,0 +1,4 @@
+// app/_context/UserContext.js or .jsx
+import { createContext } from 'react';
+
+export const UserContext = createContext();
--- app/provider.jsx
+++ app/provider.jsx
@@ -1,28 +1,23 @@
 "use client";
-import React from "react";
+import React, { Suspense } from 'react';
 //import React, { Suspense } from "react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-//import AuthProvider from "./AuthProvider";
+import AuthProvider from './AuthProvider';
 
 function Provider({ children }) {
     const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
     //console.log("Convex URL:", process.env.NEXT_PUBLIC_CONVEX_URL);
 
     return (
-        <div>
-            <ConvexProvider client={convex}>
-                {children}
-            </ConvexProvider>
-        </div>
-
-    );
-}
-
-export default Provider;
-/*        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<p>Loading...</p>}>
             <ConvexProvider client={convex}>
                 <AuthProvider>
                     {children}
-                </AuthProvider>   
+                </AuthProvider>  
             </ConvexProvider>
-        </Suspense>*/
+        </Suspense>
+
+    )
+}
+
+export default Provider;
