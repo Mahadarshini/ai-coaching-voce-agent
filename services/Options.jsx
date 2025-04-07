97edbdc9394e3d923771bc65eb4c5c7790b19d42
Maha-kg <mahadarshini290306@gmail.com>
2025-04-07 16:26:38 +0530

    Dashboard-Layout

---
+++ services/Options.jsx
@@ -0,0 +1,38 @@
+export const CoachingOption=[
+    {
+        name:'Lecture on Topic',
+        icon:'/lecture.png'
+    },
+    {
+        name:'Mock Interview',
+        icon:'/interview.png'
+    },
+    {
+        name:'Ques Ans Prep',
+        icon:'/qa.png'
+    },
+    {
+        name:'Languages Skill',
+        icon:'/language.png'
+    },
+    {
+        name:'Meditation',
+        icon:'/meditation.png'
+    },
+
+]
+
+export const CoachingExpert=[
+    {
+        name:'Joanna',
+        avatar:'/t1.png'
+    },
+    {
+        name:'Sallie',
+        avatar:'/t2.png'
+    },
+    {
+        name:'Mathhew',
+        avatar:'/t3.png'
+    }
+]
